eb9a07e1e4a0f3e6424b49819eca57b5027212f4
Hung Vu <hung.h.vu@aalto.fi>
2023-02-07 16:36:04 +0200

    Added a simple script for the practice server

---
+++ Practice/server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python# This is server.py file
+import socket
+
+s = socket.socket()
+host = socket.gethostname() # get local machine name
+port = 12345 # reserve a port for this server
+s.bind((host, port)) # bind the port
+
+s.listen(5) #  wait for client connection.
+while True:
+    c, addr = s.accept() # establish connection with client.
+    print('Got connection from', addr)
+    c.send('Hello World!'.encode())
+    c.close() # close the connection
